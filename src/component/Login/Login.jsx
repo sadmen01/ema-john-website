56f7f36b9bb474dec7fd825c13abf4a6c56bad18
sadmen rashik khan <sadmenrashik06@gmail.com>
2022-10-17 10:15:23 +0600

    login form Setup

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='form-container'>
+            <h2 className='form-titel'>Login</h2>
+            {/* <form onSubmit={handleSubmit}> */}
+            <form>
+            <div className='form-control'>
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email'required/>
+            </div>
+            <div className='form-control'>
+               <label htmlFor="password">Password</label>
+               <input type="password" name="password" required />
+            </div>
+            <input className='btn-submit' type="submit" value="login" />
+            </form>
+            <p><small>new to ema john? <Link to='/signup'><span>create a New Account</span></Link> </small></p>
         </div>
     );
 };
