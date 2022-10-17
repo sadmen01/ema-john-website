fde72276e55463549526e05b5e4ab86bf1a754b6
sadmen rashik khan <sadmenrashik06@gmail.com>
2022-10-17 13:06:44 +0600

    authcontext and link user context to index.js

---
+++ src/Context/UserContext.jsx
@@ -0,0 +1,17 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+
+const UserContext = ({children}) => {
+
+    const authInfo={}
+    return (
+    
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+    
+    );
+};
+
+export default UserContext;
--- src/component/Signup/Signup.jsx
+++ src/component/Signup/Signup.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Signup.css'
 
 const Signup = () => {
     return (
-        <div>
-            
+        <div className='form-container'>
+            <h2 className='form-titel'>Signup</h2>
+            {/* <form onSubmit={handleSubmit}> */}
+            <form>
+            <div className='form-control'>
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email'required/>
+            </div>
+            <div className='form-control'>
+               <label htmlFor="password">Password</label>
+               <input type="password" name="password" required />
+            </div>
+            <div className='form-control'>
+               <label htmlFor="confirm">Confirm Password</label>
+               <input type="password" name="password" required />
+            </div>
+            <input className='btn-submit' type="submit" value="signup" />
+            </form>
+            <p><small>Already have an account? <Link to='/login'><span>Login</span></Link> </small></p>
         </div>
     );
 };
